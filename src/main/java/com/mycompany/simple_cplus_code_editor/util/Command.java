ec538055fa1a43482ac3b6f0610f036eddb21f79
Lê Sang <42503707+LeSangDeveloper@users.noreply.github.com>
2023-06-05 15:49:40 +0800

    update command to get error

--- src/main/java/com/mycompany/simple_cplus_code_editor/util/Command.java
+++ src/main/java/com/mycompany/simple_cplus_code_editor/util/Command.java
@@ -6,6 +6,7 @@ package com.mycompany.simple_cplus_code_editor.util;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.lang.*;
@@ -18,20 +19,16 @@ public class Command {
     public String runCommand(String... command) throws InterruptedException, IOException {
     
         var processBuilder = new ProcessBuilder().command(command); 
-             
+            processBuilder.redirectErrorStream(true);
             Process process = processBuilder.start();
  
             //read the output
             InputStreamReader inputStreamReader = new InputStreamReader(process.getInputStream());
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(process.getOutputStream());
             
             StringBuilder stringb;
         try (BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
-            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
             String output = null;
             stringb = new StringBuilder();
-            bufferedWriter.write("2" + "\n");
-            bufferedWriter.flush();
             while ((output = bufferedReader.readLine()) != null) {
                 System.out.println(output);
                 stringb.append(output);
